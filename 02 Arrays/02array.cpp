2ca115edc0f3ba621667ab289f6b5654c4d29963
gks-96 <gurkiratguliani@gmail.com>
2022-02-02 13:05:58 +0530

    spaces

--- 02 Arrays/02array.cpp
+++ 02 Arrays/02array.cpp
@@ -1,37 +1,32 @@
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void printArray(int * arr,int n){
+void printArray(int *arr, int n)
+{
 
-
-	cout <<"In Function "<<sizeof(arr) <<endl;
+	cout << "In Function " << sizeof(arr) << endl;
 	arr[0] = 100;
-	for(int i=0;i<n;i++){
-		cout << arr[i] <<endl; 
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << endl;
 	}
-
 }
 
+int main()
+{
 
-int main(){
+	int arr[] = {1, 2, 3, 4, 5, 6};
 
-	int arr[] = {1,2,3,4,5,6};
+	int n = sizeof(arr) / sizeof(int);
 
-	int n = sizeof(arr)/sizeof(int);
+	printArray(arr, n);
 
-
-	
-	printArray(arr,n);
-
-	
-	cout <<"In Main "<<sizeof(arr) <<endl;
-	for(int i=0;i<n;i++){
-		cout << arr[i] <<endl; 
+	cout << "In Main " << sizeof(arr) << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << endl;
 	}
-
-	
-
 
 	return 0;
 }
